81450e35d04cdd0841b57dc989311e19b59119fd
jenry cacerez guzman <jenrybrown@hotmail.com>
2019-08-31 19:14:04 +0100

    SUBIENDO TAREA 1

---
+++ ejercicio1/proyecto1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace proyecto1
+{
+    class proyecto1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("jenry Cacerez");
+        }
+    }
+}
