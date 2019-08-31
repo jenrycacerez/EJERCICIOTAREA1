55a538c75adb81056703c3447ddf3fa3897f4c71
jenry cacerez guzman <jenrybrown@hotmail.com>
2019-08-31 19:07:15 +0100

    SUBIENDO 2

---
+++ ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jenry Cacerez Guzman");
+            Console.WriteLine("Tengo 22 Años");
+            Console.WriteLine("soy estudiante de informatica!");
+            Console.WriteLine("Me gusta los deporte y compartir con mis amigos");
+
+        }
+    }
+}
